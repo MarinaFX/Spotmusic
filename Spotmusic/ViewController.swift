3666cd5a5974fc1de39f00f00dcf1a902e6b6311
Marina De Pazzi <pazzi.dev@gmail.com>
2021-06-18 13:41:00 -0300

    chore: project creation + gitignore

---
+++ Spotmusic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spotmusic
+//
+//  Created by Marina De Pazzi on 18/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
