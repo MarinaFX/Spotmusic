93391b00cbf61aa333be8594aee4285e6e19c40c
Marina De Pazzi <pazzi.dev@gmail.com>
2021-06-28 16:56:17 -0300

    chore: removing duplicate files

--- Spotmusic/ViewControllers/PlayingViewController.swift
+++ Spotmusic/ViewControllers/PlayingViewController.swift
@@ -8,43 +8,34 @@
 import UIKit
 
 class PlayingViewController: UIViewController, SongFromAlbumDelegate {
-    func favoriteSong(song: Music) -> Bool {
-        return false
-    }
-    
-    
-    var musicService: MusicService? = try? MusicService()
- 
+    //MARK: Variables and class setup
     @IBOutlet weak var playingCoverImage: UIImageView!
-    
     @IBOutlet weak var titleLabel: UILabel!
-    
     @IBOutlet weak var artistLabel: UILabel!
-    
     @IBOutlet weak var likedImage: UIImageView!
+    
+    private var musicService: MusicService? = try? MusicService()
     
     var delegate: SongFromAlbumDelegate?
     var playingNow: Music?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-     
+        
         playingCoverImage.image = musicService?.getCoverImage(forItemIded: playingNow?.id ?? "")
         
         titleLabel.text = playingNow?.title
         artistLabel.text = playingNow?.artist
         
-
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "list.dash"), style: .plain, target: self, action: #selector(addTapped))
         navigationItem.rightBarButtonItem?.tintColor = UIColor.green
         
         let tap = UITapGestureRecognizer(target: self, action: #selector(tappedMe))
         likedImage.addGestureRecognizer(tap)
         likedImage.isUserInteractionEnabled = true
-        
-        
     }
+    
+    
     @objc func tappedMe()
     {
         guard let isFavorite = delegate?.favoriteSong(song: playingNow!) else { return }
@@ -55,6 +46,8 @@ class PlayingViewController: UIViewController, SongFromAlbumDelegate {
         }
         else {
             likedImage.image = UIImage(systemName: "heart")
+            likedImage.tintColor = UIColor.black
+
         }
     }
     
@@ -64,16 +57,18 @@ class PlayingViewController: UIViewController, SongFromAlbumDelegate {
         performSegue(withIdentifier: "toQueueList", sender: playingNow)
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "toQueueList", let music = sender as? Music {
-            let destination = segue.destination as? UINavigationController
-            let dest2 = destination?.topViewController as? QueueListViewController
-            
-            dest2?.nowPlayingMusic = music
-            
-        }
+    func favoriteSong(song: Music) -> Bool {
+        return false
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toQueueList", let music = sender as? Music {
+            let dest1 = segue.destination as! UINavigationController
+            let finalDest = dest1.topViewController as! QueueListViewController
 
+            finalDest.nowPlayingMusic = music
+
+        }
+    }
 }
 
