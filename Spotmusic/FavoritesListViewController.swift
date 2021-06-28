81673b1fb09659da1c72f8b1e28960e938bc38a8
Marina De Pazzi <pazzi.dev@gmail.com>
2021-06-28 16:00:55 -0300

    funciona inferno 2

--- Spotmusic/FavoritesListViewController.swift
+++ Spotmusic/FavoritesListViewController.swift
@@ -9,7 +9,7 @@ import Foundation
 import UIKit
 
 class FavoritesListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    
+    //lalalalal
     //MARK: Variables and class setup
     @IBOutlet weak var tableView: UITableView!
     
